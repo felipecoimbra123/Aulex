ea55b1765ddaee88ddc1c9baddd0c70f3eea25bb
felipecoimbra123 <felipe.2007coimbra@gmail.com>
2025-09-17 23:52:13 -0300

    Configuração do banco de dados

---
+++ db.sql
@@ -0,0 +1,40 @@
+create database aulex;
+use aulex;
+
+create table usuarios(
+	id int primary key auto_increment,
+    nome varchar(255) not null unique,
+    email varchar(255) not null unique,
+    cpf varchar(11) not null unique,
+    senha varchar(255) not null unique,
+    papel enum('aluno', 'professor', 'pedagogo')
+);
+
+create table usuario_has_turma(
+	id int primary key auto_increment,
+    usuario_id int not null,
+    turma_id int not null,
+    foreign key (usuario_id) references usuarios(id),
+    foreign key (turma_id) references turmas(id)
+);
+
+create table turmas(
+	id int primary key auto_increment,
+    nome varchar(255) not null,
+    turno varchar(255) not null
+);
+
+create table presencas(
+	id int primary key auto_increment,
+    status enum('presente', 'falta'),
+	usuario_id int not null,
+    turma_id int not null,
+    foreign key (usuario_id) references usuarios(id),
+    foreign key (turma_id) references turmas(id)
+);
+
+create table notas(
+	id int primary key auto_increment,
+	usuario_id int not null,
+    foreign key (usuario_id) references usuarios(id)
+);
